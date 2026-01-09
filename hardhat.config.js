0f7a5ed785516a87bdb4a3239d77e55e49df8b3e
Sriram Vasamsetti <23P31A1232@acet.ac.in>
2026-01-09 20:25:51 +0530

    Add hardhat configuration

---
+++ hardhat.config.js
@@ -0,0 +1,20 @@
+require('@nomicfoundation/hardhat-toolbox');
+
+module.exports = {
+  solidity: '0.8.0',
+  networks: {
+    hardhat: {
+      chainId: 31337,
+      loggingEnabled: true,
+    },
+    localhost: {
+      url: 'http://127.0.0.1:8545',
+      chainId: 31337,
+    },
+  },
+  paths: {
+    sources: './contracts',
+    tests: './tests',
+    artifacts: './artifacts',
+  },
+};
